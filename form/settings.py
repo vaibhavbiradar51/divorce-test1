d8d30b6fb950c37a36f9db72b84357eee0732c05
vaibhavbiradar51 <48184786+vaibhavbiradar51@users.noreply.github.com>
2021-02-18 14:23:44 +0530

    Update settings.py

--- form/settings.py
+++ form/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/admin/'
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
