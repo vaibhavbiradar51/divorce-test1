ce7062dbc90e1d8dc879d91730a8c9192e3bb8d3
Bhaskar Praveen Naidu <45845846+bhaskarpraveen@users.noreply.github.com>
2021-02-19 08:41:43 +0530

    Update models.py

--- divorce/models.py
+++ divorce/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 class Divorce(models.Model):
     sno = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, null=True)
-    State_issued_divorce_id = models.CharField(max_length=100, null=True)
-    Date_of_birth = models.CharField(max_length=100, null=True)
-    Nationality = models.CharField(max_length=100, null=True)
-    Maritial_status = models.CharField(max_length=100, null=True)
-    Highest_education = models.CharField(max_length=100, null=True)
+    name = models.CharField(max_length=100, null=True,blank=True)
+    State_issued_divorce_id = models.CharField(max_length=100, null=True,blank=True)
+    Date_of_birth = models.CharField(max_length=100, null=True,blank=True)
+    Nationality = models.CharField(max_length=100, null=True,blank=True)
+    Maritial_status = models.CharField(max_length=100, null=True,blank=True)
+    Highest_education = models.CharField(max_length=100, null=True,blank=True)
 
     def __str__(self):
-    	return str(self.sno)
+    	return 'Record - '+str(self.sno)
