92439bd15d0374917fc6f0eac7131b088b405704
Bhaskar Praveen Naidu <45845846+bhaskarpraveen@users.noreply.github.com>
2021-02-19 08:47:20 +0530

    Update admin.py

--- divorce/admin.py
+++ divorce/admin.py
@@ -4,7 +4,7 @@ from divorce.models import Divorce
 # Register your models here.
 class DivorceAdmin(admin.ModelAdmin):
     model = Divorce
-    list_display = ['name','State_issued_divorce_id','Date_of_birth','Nationality','Maritial_status','Highest_education']
+    list_display = ['sno','name','State_issued_divorce_id','Date_of_birth','Nationality','Maritial_status','Highest_education']
     search_fields=('name','State_issued_divorce_id','Date_of_birth','Nationality','Maritial_status','Highest_education') 
 admin.site.register(Divorce,DivorceAdmin)
 
